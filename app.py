73d1282820fce27672d7aaff0f9566d9fe8d9569
fizzathzunera-cyber <fizzathzunera@gmail.com>
2026-04-01 09:50:01 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
 from flask import Flask, render_template, request
 import sqlite3
+import os
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     return render_template('index.html')
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT",5000))
+    app.run(host="0.0.0.0", port=port)
 
 @app.route('/submit', methods=['POST'])
 def submit():
